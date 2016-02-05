41fa03285be7f9408ee6b29f4b8e966a725fccc6
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-05 10:53:29 -0500

    testing the setting and getting of variables

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Starting Password Manager");
+
+var storage = require('node-persist');
+storage.initSync();
+
+//storage.setItemSync( 'name', 'Marcus');
+var name = storage.getItemSync('name');
+
+console.log('Saved name is: ' + name);
